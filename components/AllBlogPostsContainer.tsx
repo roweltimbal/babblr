0ba70ae6530ce02c586691f1733b58bf0e1642da
Rowel Timbal <practice_practice25@yahoo.com>
2026-02-15 18:34:39 -0500

    Add search bar feature with filtering

--- components/AllBlogPostsContainer.tsx
+++ components/AllBlogPostsContainer.tsx
@@ -1,18 +1,25 @@
+'use client'
 import { BlogCard } from "./Card"
 import type { Blog } from "@/types/blog";
 import BlogToolBar from "./BlogToolbar";
+import { useSearchParams } from "next/navigation";
 
 type AllBlogPostsProps = {
   blogs: Blog[];
 };
 
 export function AllBlogPostContainer ({blogs}: AllBlogPostsProps) {
+    const searchParams = useSearchParams();
+    const searchKeyword = searchParams?.get('search') || '';
+    const filteredBlogs = blogs.filter(blog => blog.title.toLowerCase().includes(searchKeyword))
+    console.log(filteredBlogs)
+
     return(
         <div className="w-full">
             <BlogToolBar/>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 gap-y-6">
                 {
-                    blogs.map(blog => (
+                    filteredBlogs.map(blog => (
                         <BlogCard key={blog.id} blog={blog}/>
                     ))
                 }
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,12 +1,36 @@
+'use client'
+import { useState } from "react"
+import { useSearchParams, useRouter } from "next/navigation"
 import { Button } from "@/components/ui/button"
 import { Field } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
 
 export function InputInline() {
+  const params = useSearchParams();
+  const router = useRouter();
+
+  const [keyword, SetKeyword] = useState(params.get('search') || '');
+  const handleClick = () => {
+    const newParams = new URLSearchParams(params.toString());
+    if(keyword) {
+      newParams.set('search', keyword)
+    } else {
+      newParams.delete('search')
+    }
+    router.push(`?${newParams.toString()}`)
+  }
+
   return (
     <Field orientation="horizontal">
-      <Input type="search" placeholder="Search..."  />
-      <Button size='sm' className="bg-foreground hover:bg-accent">Search</Button>
+      <Input type="search" placeholder="Search..." value={keyword}
+      onChange={(e) => {
+        SetKeyword(e.target.value.toLowerCase());
+      }}
+      onKeyDown={(e) => {
+        if(e.key === 'Enter') handleClick()
+      }}
+      />
+      <Button size='sm' className="bg-foreground hover:bg-accent" onClick={handleClick}>Search</Button>
     </Field>
   )
 }
